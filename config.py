d4b7e5579247d257566b50645e06b1fe448982e7
Jose Carlos Pazos <jcpazos93@gmail.com>
2022-08-23 19:00:43 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
         'postgresql://postgres:UTEC@localhost:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
